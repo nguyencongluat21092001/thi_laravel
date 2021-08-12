bf32b1e5e6a12ef2132a18288637a0abba639a65
nguyencongluat21092001 <luatncth2007024@fpt.edu.vn>
2021-08-12 19:47:06 +0700

    “”

--- database/migrations/2021_08_12_115337_create_tasks_table.php
+++ database/migrations/2021_08_12_115337_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasksTable extends Migration
+class CreatTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+        //
+        Schema::create('tasks',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
